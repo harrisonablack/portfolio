cca618418453ba670877cb514dec7d96f6a01049
Harrison Black <harrison.black18@gmail.com>
2024-06-25 15:36:58 +1000

    typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
       <main className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <section>
           <div className="flex-auto min-w-0 mt-6 flex-col px-2 md:px-0">
-            <h1 className="mb-8 text-2xl font-semibold tracking-tighter">Hi, Im Harrison</h1>
-            <p className="mb-4">Im a junior developer studying at RMIT, based in Melbourne, Australia</p>
+            <h1 className="mb-8 text-2xl font-semibold tracking-tighter">Hi, I'm Harrison</h1>
+            <p className="mb-4">I'm a junior developer studying at RMIT, based in Melbourne, Australia</p>
           </div>
         </section>
         <footer className="mb-16">
